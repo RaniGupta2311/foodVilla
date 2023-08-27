7ae571f19b8991195adb10a95fa57495b109e59b
Rani Gupta <rani.gupta259@gmail.com>
2023-08-27 11:43:45 +0530

    tailwind config and header

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
 // console.log("Namaste Food");
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-const App=()=>{
+import Header from "./src/components/Header"
+import Body from "./src/components/Body";
+import Footer from "./src/components/Footer"
+const AppLayout=()=>{
     return (
-    <div>
-        <h1>Hello ReactJs!! You are doing great</h1>
-    </div>
+        <>
+        {/* <h1>Hello ReactJs!! You are doing great</h1> */}
+            <Header/>
+            <Body/>
+            <Footer/>
+        </>
     )
 
 }
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(<AppLayout/>);
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+// import Logo from "../utils/images/images";
+const Title=()=>{
+    return (
+        <div className="w-24 h-24">
+            <a href="/"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8jztgjfG6PTf2pgEu0ycmof2syvuQ40a8bw&usqp=CAU" alt="logo" className="rounded-full"/></a>
+        </div>
+    )
+}
+const Header=()=>{
+    return (
+        <div className="h-24 flex justify-between items-center bg-amber-400 pr-3 sm:pr-8 sm:pl-2 md:pr-4 lg:pr-8 shadow-lg">
+            <Title/>
+            <div>
+                <ul className="flex gap-3 sm:gap-5 text-white text-lg font-bold tracking-wide md:tracking-widest ">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Header;
