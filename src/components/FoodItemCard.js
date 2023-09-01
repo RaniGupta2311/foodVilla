75e2562a21b5066f8794bbac083b7c76a6f23b49
Rani Gupta <rani.gupta259@gmail.com>
2023-09-01 18:40:12 +0530

    food item

--- src/components/FoodItemCard.js
+++ src/components/FoodItemCard.js
@@ -3,6 +3,7 @@ import { IMG_CDN_URL } from "../utils/Constants";
 const FoodItemCard=({name,description,imageId,price,isVeg})=>{
     return (
         <div className="flex pt-5 border-t-2 mt-3 justify-between w-full gap-1 sm:gap-4 md:gap-12 lg:gap-16 h-52 md:h-40">
+        
             <div className="w-[16rem] md:w-[26rem] h-36">
             {isVeg?<h1 className="border-[1px] border-green-600 inline-block text-xs p-[1px]">🟢</h1>:<h1 className="border-[1px] border-red-600 inline-block text-xs p-[1px]">🔺</h1>}
             <h1 className="text-md font-light mt-1">{name}</h1>
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,12 +13,12 @@ const Header=()=>{
         <div className="h-24 w-[100%] flex justify-between items-center bg-amber-400 pr-3 sm:pr-8 sm:pl-2 md:pr-4 lg:pr-8 shadow-xl fixed top-0 z-50">
             <Title/>
             <div>
-                <ul className="flex gap-3 sm:gap-5 text-white text-lg font-semibold tracking-wide md:tracking-widest ">
-                    <Link to="/"><li>Home</li></Link>
-                    <Link to="/about"><li>About</li></Link>
-                    <Link to="/contact"><li>Contact</li></Link>
-                    <Link to="/cart"><li>Cart</li></Link>
-                    <Link to="/login"><li>Login</li></Link>
+                <ul className="flex gap-1 sm:gap-5 text-white text-lg font-semibold tracking-wide md:tracking-widest ">
+                    <Link to="/"><li className="hover:bg-gray-400 pl-2 pr-2 pt-1 pb-1 rounded-md">Home</li></Link>
+                    <Link to="/about"><li className="hover:bg-gray-400 pl-2 pr-2 pt-1 pb-1 rounded-md">About</li></Link>
+                    <Link to="/contact"><li className="hover:bg-gray-400 pl-2 pr-2 pt-1 pb-1 rounded-md">Contact</li></Link>
+                    <Link to="/cart"><li className="hover:bg-gray-400 pl-2 pr-2 pt-1 pb-1 rounded-md">Cart</li></Link>
+                    <Link to="/login"><li className="hover:bg-gray-400 pl-2 pr-2 pt-1 pb-1 rounded-md">Login</li></Link>
                 </ul>
             </div>
         </div>
